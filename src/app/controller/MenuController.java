ced47dad3ee5e6dd5416c415a9d15dd072342c94
AspectLeft <aspectleft@gmail.com>
2020-04-11 13:35:03 -0700

    Update MenuController.java
    Added a modal with progress bar to block the UI when loading the files.

--- src/app/controller/MenuController.java
+++ src/app/controller/MenuController.java
@@ -1,10 +1,18 @@
 package app.controller;
 
 import app.model.DataModel;
+import javafx.concurrent.Task;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.HBox;
+import javafx.stage.*;
 
 import java.io.File;
 
@@ -27,11 +35,68 @@ public class MenuController {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDir = directoryChooser.showDialog(menuBar.getScene().getWindow());
         if (selectedDir == null) return;
-        model.loadData(selectedDir);
+
+        ProgressForm progressForm = new ProgressForm();
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                model.loadData(selectedDir);
+                return null;
+            }
+        };
+        progressForm.activateProgressBar(task);
+        task.setOnSucceeded(event -> {
+            progressForm.getDialogStage().close();
+        });
+
+        progressForm.getDialogStage().show();
+
+        Thread thread = new Thread(task);
+        thread.start();
     }
 
     @FXML
     public void exit() {
         menuBar.getScene().getWindow().hide();
+    }
+
+
+    public static class ProgressForm {
+        private final Stage dialogStage;
+        private final ProgressBar pb = new ProgressBar();
+        private final ProgressIndicator pin = new ProgressIndicator();
+
+        public ProgressForm() {
+            dialogStage = new Stage();
+            dialogStage.initStyle(StageStyle.UTILITY);
+            dialogStage.setResizable(false);
+            dialogStage.initModality(Modality.APPLICATION_MODAL);
+            dialogStage.setOnCloseRequest(Event::consume);
+
+            // PROGRESS BAR
+            final Label label = new Label();
+            label.setText("alerto");
+
+            pb.setProgress(-1F);
+            pin.setProgress(-1F);
+
+            final HBox hb = new HBox();
+            hb.setSpacing(5);
+            hb.setAlignment(Pos.CENTER);
+            hb.getChildren().addAll(pb, pin);
+
+            Scene scene = new Scene(hb);
+            dialogStage.setScene(scene);
+        }
+
+        public void activateProgressBar(final Task<?> task)  {
+            pb.progressProperty().bind(task.progressProperty());
+            pin.progressProperty().bind(task.progressProperty());
+            dialogStage.show();
+        }
+
+        public Stage getDialogStage() {
+            return dialogStage;
+        }
     }
 }
